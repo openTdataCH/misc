cd2daccf21adce0df7fb80497ae6acbaf601529c
ue71603 <matthias.guenter@sbb.ch>
2016-11-28 15:52:10 +0100

    Create load.cmd

---
+++ actual_data/postgres_load/load.cmd
@@ -0,0 +1,21 @@
+REM  Loading "actual data" from opentransportdata.swiss into a postgres database 
+REM Christian Trachsel, 2016
+REM 
+REM More information: 
+REM - https://opentransportdata.swiss/de/dataset/istdaten
+REM - https://opentransportdata.swiss/de/cookbook/ist-daten/
+
+@echo off
+
+setlocal
+
+set PGPASSWORD=xxxxx
+
+for %%f in (*.csv)  do psql -d IstDaten -h localhost -U postgres -p
+5432 -c "COPY public.ist_daten FROM '%%~dpnxf'  WITH DELIMITER ';'
+CSV HEADER;"
+
+pause
+
+endlocal
+

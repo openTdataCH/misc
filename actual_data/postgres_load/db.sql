92718f51a45156e5359aaf3b2d7deabd35217a7b
ue71603 <matthias.guenter@sbb.ch>
2016-11-28 15:45:06 +0100

    Create db.sql

---
+++ actual_data/postgres_load/db.sql
@@ -0,0 +1,29 @@
+/*
+Data creation from ODPCH öV Schweiz Actual Data files
+Christian Trachsel, 2016
+
+Relevant data set: https://opentransportdata.swiss/dataset/istdaten 
+More information: https://opentransportdata.swiss/cookbook/ist-daten/
+*/
+CREATE TABLE IST_DATEN(   
+BETRIEBSTAG         DATE,   
+FAHRT_BEZEICHNER    VARCHAR(50),   
+BETREIBER_ID        VARCHAR(20),   
+BETREIBER_ABK       VARCHAR(10),   
+BETREIBER_NAME      VARCHAR(100),   
+PRODUKT_ID          VARCHAR(10),   
+LINIEN_ID           INT,   
+LINIEN_TEXT         VARCHAR(10),  
+UMLAUF_ID           VARCHAR(8),   
+VERKEHRSMITTEL_TEXT VARCHAR(10),   
+ZUSATZFAHRT_TF      BOOLEAN,   
+FAELLT_AUS_TF       BOOLEAN,   
+BPUIC               INT,   
+HALTESTELLEN_NAME   VARCHAR(50),   
+ANKUNFTSZEIT        TIMESTAMP,   
+AN_PROGNOSE         TIMESTAMP,   
+AN_PROGNOSE_STATUS  VARCHAR(12),  
+ABFAHRTSZEIT        TIMESTAMP,   
+AB_PROGNOSE         TIMESTAMP,   
+AB_PROGNOSE_STATUS  VARCHAR(12),   
+DURCHFAHRT_TF       BOOLEAN);
